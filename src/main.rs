003541a18241bad7a14b4c4cc505c4bc9b762356
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-17 22:14:48 -0700

    Add docs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 use std::fs;
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "mahou", about = "A programming language")]
-struct Opt {
-    #[structopt(short, long)]
-    verbose: bool,
-
-    /// The input file to be interpreted
-    filename: String,
-}
-
 #[doc = "Syntax"]
 /**
     Example:
@@ -35,6 +25,17 @@ struct Opt {
     - $E
 */
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "mahou", about = "A programming language")]
+struct Opt {
+    #[structopt(short, long)]
+    verbose: bool,
+
+    /// The input file to be interpreted
+    filename: String,
+}
+
+/// Check if a given character is whitespace
 fn is_char_whitespace(ch: char) -> bool {
     match ch {
         '\t' | ' ' | '\n' => true,
@@ -42,25 +43,28 @@ fn is_char_whitespace(ch: char) -> bool {
     }
 }
 
-fn is_char_operator(ch: char) -> bool {
+/// Check if a character is an symbol
+fn is_char_symbol(ch: char) -> bool {
     match ch {
         '+' | '-' | '*' | '/' | '>' | '<' | '=' | ';' | '$' => true,
         _ => false,
     }
 }
 
+/// Check if a character is in between 0 and 9
 fn is_char_numeric(ch: char) -> bool {
     return ch.is_digit(10);
 }
 
+/// Check if the current character or the next character will end the token
 fn ends_token(cur: char, next: char) -> bool {
     if is_char_whitespace(next) {
         return true;
     }
-    if is_char_operator(cur) {
+    if is_char_symbol(cur) {
         return true;
     }
-    if is_char_operator(next) {
+    if is_char_symbol(next) {
         return true;
     }
     if is_char_whitespace(cur) {
@@ -97,6 +101,7 @@ struct Token {
     token: Tokens,
 }
 
+/// Given a string reference that has been identified as a single token, find what token it is
 fn tokenize(part: &str) -> Token {
     let mut token: Tokens = match part {
         "-" => Tokens::Minus,
@@ -112,6 +117,8 @@ fn tokenize(part: &str) -> Token {
         _ => Tokens::Identifier,
     };
 
+    // Both identifiers and numeric literals get assigned Identifier
+    // but this check if each character in the identifier is a number
     if token == Tokens::Identifier {
         for c in part.chars() {
             if is_char_numeric(c) {
@@ -144,11 +151,13 @@ struct Lexer {
 }
 
 impl Lex for Lexer {
+    /// Change what the character is by shifting them
     fn move_pointer(&mut self) {
         self.previous_char = self.current_char;
         self.current_char = self.next_char;
         self.next_char = self.chars[self.index];
     }
+    /// Increment the index before or after the point moves depending on if increment is Before or After
     fn next(&mut self, increment: Increment) {
         if increment == Increment::Before {
             self.index += 1;
@@ -159,6 +168,7 @@ impl Lex for Lexer {
         }
     }
     fn lexer(&mut self) {
+        // Get all the chars from the contents of the file
         self.chars = self.contents.chars().collect();
         let mut current_part: String = String::new();
 
@@ -166,8 +176,11 @@ impl Lex for Lexer {
         let chars_len: usize = self.contents.len();
 
         while self.index + 1 <= chars_len {
+            // If the character is not whitespace, push it to the current part
             if !is_char_whitespace(self.current_char) {
                 current_part.push(self.current_char);
+                // If the current character or the next ends the token
+                // push the current part as a token, then reset the part
                 if ends_token(self.current_char, self.next_char) {
                     self.tokens.push(tokenize(&current_part));
                     current_part = String::new();
@@ -179,7 +192,7 @@ impl Lex for Lexer {
 }
 
 fn new_lexer(contents: &str) -> Lexer {
-    let lexer = Lexer {
+    let lexer: Lexer = Lexer {
         contents: contents.to_string(),
         chars: Vec::new(),
         index: 0,
