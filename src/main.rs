98b5b441a6b73a5a8620016136ec7d682c020114
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-05-17 20:05:02 -0700

    Add syntax, and init

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#[doc="Syntax"]
+/**
+    Example:
+    - set a = 0;
+    - $a += 1;
+    - print $a;
+    - jump -2;
+
+    Commands:
+    - set
+    - jump
+    - print
+
+    Operators:
+    - Plus (+)
+    - Minus (-)
+    - Divide (/)
+    - Multiply (*)
+
+    Constants:
+    - $PI
+    - $E
+*/
+
+
+fn main() {
+    println!("Hello, world!");
+}
